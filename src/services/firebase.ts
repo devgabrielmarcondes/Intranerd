986219f5cb934af81da8b9fce255c9b0a6dce710
Gabriel Marcondes dos Santos <devgabmasantos@gmail.com>
2021-12-07 11:01:13 +0000

    Firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyA2PXgF2kI_La71tx-0YalL1SnvqoeUhws",
+  authDomain: "AIzaSyA2PXgF2kI_La71tx-0YalL1SnvqoeUhws",
+  databaseURL: "https://intranerd-da46a-default-rtdb.firebaseio.com",
+  projectId: "intranerd-da46a",
+  storageBucket: "intranerd-da46a.appspot.com",
+  messagingSenderId: "605339050720",
+  appId: "1:605339050720:web:d2a02c3098dd8b6fa76685" 
 };
 
 const app = firebase.initializeApp(firebaseConfig);
@@ -22,7 +22,7 @@ export { firebase };
 
 /*
 REACT_APP_API_KEY="AIzaSyA2PXgF2kI_La71tx-0YalL1SnvqoeUhws"
-REACT_APP_AUTH_DOMAIN="intranerd-da46a.firebaseapp.com"
+REACT_APP_AUTH_DOMAIN="AIzaSyA2PXgF2kI_La71tx-0YalL1SnvqoeUhws"
 REACT_APP_DATABASE_URL="https://intranerd-da46a-default-rtdb.firebaseio.com"
 REACT_APP_PROJECT_ID="intranerd-da46a"
 REACT_APP_STORAGE_BUCKET="intranerd-da46a.appspot.com"
